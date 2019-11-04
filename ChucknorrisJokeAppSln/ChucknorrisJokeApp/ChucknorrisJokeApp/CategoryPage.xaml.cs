292bebe84bdada6a6a119c23d70c5c4e7ed66c5f
luthando Sitole <luthando@quirky30.co.za>
2019-11-04 16:04:01 +0200

    my chucknorriesApp

---
+++ ChucknorrisJokeAppSln/ChucknorrisJokeApp/ChucknorrisJokeApp/CategoryPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ChucksNorriesLibrary;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChucknorrisJokeApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CategoryPage : ContentPage
+    {
+        public string[] Categories { get; set; }
+        public CategoryPage()
+        {
+            InitializeComponent();
+        }
+
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var jokeGen = new JokeGernarator();
+            Categories = await jokeGen.GetCatagories();
+            BindingContext = this;
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            string Categories = (sender as Label).Text; 
+            HttpClient Client = new HttpClient();
+            var Category = await Client.GetStringAsync("https://api.chucknorris.io/jokes/random?category=" + Categories);
+            var CategoryConvert = JsonConvert.DeserializeObject<Joke>(Category);
+            var realJoke = CategoryConvert.value;
+            await DisplayAlert(Categories, realJoke, "ok");
+        }
+    }
+}
